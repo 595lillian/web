e9cb5ddcf88fa1c73e1dc55ce70bbacc1b326552
595lillian <2397865099@qq.com>
2024-03-20 17:25:06 +0800

    web commit

---
+++ es-demo/src/index.js
@@ -0,0 +1,15 @@
+// function deepCopy(obj) {
+//     if (Array.isArray(obj)) {
+//         return obj.map(item => deepCopy(item));
+//     } else if (obj && typeof obj === 'object') {
+//         let newObj = {};
+//         for (let key in obj) {
+//             if (obj.hasOwnProperty(key)) {
+//                 newObj[key] = deepCopy(obj[key]);
+//             }
+//         }
+//         return newObj;
+//     } else {
+//         return obj;
+//     }
+// }
